f8c5afa61d9fc58b54bca56b030162e7eacf8729
Lukas Nitzsche <lukasnitzsche@yahoo.com>
2023-12-02 15:09:29 +0100

    day 2 failed

---
+++ 2023/02/02.py
@@ -0,0 +1,44 @@
+# NOT MY CODE, I FAILED THIS DAY
+# I need to learn regex basically. It's not do-able without it.
+
+from pathlib import Path
+import re
+import math
+
+gameRegex = re.compile(r"Game (\d+): (.*)$")
+colorRegex = re.compile(r"(\d+) (.*)")
+limits = {"red": 12, "green": 13, "blue": 14}
+
+def part1(line):
+    match = gameRegex.search(line)
+    for color in match[2].replace(";", ",").split(", "):
+        colorMatch = colorRegex.search(color)
+        if int(colorMatch[1]) > limits[colorMatch[2]]:
+            return 0
+
+    return int(match[1])
+
+
+def part2(line):
+    match = gameRegex.search(line)
+    minimas = {"red": 0, "green": 0, "blue": 0}
+    for color in match[2].replace(";", ",").split(", "):
+        colorMatch = colorRegex.search(color)
+        value = int(colorMatch[1])
+        color = colorMatch[2]
+        if value > minimas[color]:
+            minimas[color] = value
+
+    return minimas["red"] * minimas["green"] * minimas["blue"]
+
+def main():
+    path = Path(__file__).parent / "./input_1.txt"
+    with path.open() as f:
+        lines = f.readlines()
+        result = [part1(line) for line in lines]
+        print("part1: ", sum(result))
+        result = [part2(line) for line in lines]
+        print("part2: ", sum(result))
+
+if __name__ == "__main__":
+    main()
